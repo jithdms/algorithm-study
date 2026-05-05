e4a4ccb27dbb42c625654ef119dd25343f2458bb
jithdms <jithdms@gmail.com>
2026-05-05 15:30:54 +0900

    Create 26-50.c

---
+++ CodeUp/기초 100제/26-50.c
@@ -0,0 +1,166 @@
+#include <stdio.h>
+
+/*
+//26번
+int main()
+{
+    int a;
+    int b;
+    int c;
+    
+    scanf("%d:%d:%d", &a, &b, &c);
+    printf("%d", b);
+
+    return 0;
+}
+
+//27번
+int main()
+{
+    int a;
+    int b;
+    int c;
+    
+    scanf("%04d.%04d.%02d", &a, &b, &c);
+    printf("%02d-%02d-%04d", c, b, a);
+
+    return 0;
+}
+
+//28일
+int main()
+{
+    unsigned int a;
+    
+    scanf("%u", &a);
+    printf("%u", a);
+
+    return 0;
+}
+
+//30번
+int main()
+{
+    long long int a;
+    
+    scanf("%lld", &a);
+    printf("%lld", a);
+
+    return 0;
+}
+
+//32번
+int main()
+{
+    int a;
+    
+    scanf("%d", &a);
+    printf("%o", a);
+
+    return 0;
+}
+
+//32번
+int main()
+{
+    int a;
+    
+    scanf("%d", &a);
+    printf("%x", a);
+
+    return 0;
+}
+
+//33번
+int main()
+{
+    int a;
+    
+    scanf("%d", &a);
+    printf("%X", a);
+
+    return 0;
+}
+
+//34번
+int main()
+{
+    int a;
+    
+    scanf("%o", &a);
+    printf("%d", a);
+
+    return 0;
+}
+
+//35번
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    
+    scanf("%x", &a);
+    printf("%o", a);
+
+    return 0;
+}
+
+//36번
+int main()
+{
+    char a;
+    
+    scanf("%c", &a);
+    printf("%d", a);
+
+    return 0;
+}
+
+//37번
+int main()
+{
+    int a;
+    
+    scanf("%d", &a);
+    printf("%c", a);
+
+    return 0;
+}
+
+//38번
+int main()
+{
+    long long int a;
+    long long int b;
+    
+    scanf("%lld %lld", &a, &b);
+    printf("%lld", a+b);
+
+    return 0;
+}
+
+//39번
+int main()
+{
+    long long int a;
+    long long int b;
+    
+    scanf("%lld %lld", &a, &b);
+    printf("%lld", a+b);
+
+    return 0;
+}
+*/
+
+//40번
+int main()
+{
+    int a;
+    int b;
+    
+    scanf("%d", &a);
+    printf("%d", -a);
+
+    return 0;
+}
